69ae6704c52104e3791e0d4d238586d65c39f303
DanielRautavalta <daniel.rautavalta@eduespoo.fi>
2020-03-06 10:00:27 +0200

    Tallennus 1.

---
+++ src/kahvila/Asiakas.java
@@ -0,0 +1,20 @@
+package kahvila;
+
+public class Asiakas {
+    private int raha;
+    public int Asiakas(int raha) {
+        this.raha = raha;
+    }
+
+    public int getRaha() {
+        int raha = this.raha;
+        return this.raha;
+    }
+
+    public void otaRaha(int maksu) {
+        this.raha = this.raha - raha;
+    }
+
+
+
+}
---
+++ src/kahvila/Kahvila.java
@@ -0,0 +1,6 @@
+import java.util.*;
+package kahvila;
+
+public class Kahvila {
+    ArrayList tuotteet = new ArrayList()
+}
