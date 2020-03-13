7f8acef8aaff824d2ae5eece3fee442d3c393fb3
DanielRautavalta <daniel.rautavalta@eduespoo.fi>
2020-03-13 09:41:59 +0200

    ads

--- src/kahvila/Kahvila.java
+++ src/kahvila/Kahvila.java
@@ -1,9 +1,35 @@
 package kahvila;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Kahvila {
-    ArrayList tuotteet = new ArrayList();
+    public List<Tuote> tuotteet;
+
+
+    public Kahvila() {
+        this.tuotteet = new ArrayList<>();
+    }
+
+
+    public void LisaaTuote(Tuote c) {
+        tuotteet.add(c);
+    }
+
+
+    public void poistaTuote(int a) {
+        tuotteet.remove(a);
+    }
+
+
+    public int getIndex(String poisto) {
+        return tuotteet.indexOf(poisto);
+    }
+
+
+    public int suuri() {
+        return tuotteet.size();
+    }
 
 
 
--- src/kahvila/Main.java
+++ src/kahvila/Main.java
@@ -1,6 +1,33 @@
 package kahvila;
-    public class Main {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+
+    class Main {
     public static void main(String[] args) {
-        System.out.println("#")
+        Kahvila kahvila = new Kahvila();
+        Kahvila tuotteet = new Kahvila();
+        Scanner sc = new Scanner(System.in);
+        Iterator iterator = tuotteet.iterator()
+        while (true) {
+            System.out.println("Tervetuloa kahvilaan!");
+
+            System.out.println("Syötä käyttäjätunnus:");
+            String käyttäjä = sc.nextLine();
+
+            System.out.println("Syötä salasana:");
+            String salasana = sc.nextLine();
+            if (käyttäjä.equals("Omistaja") && (salasana.equals("Salasana")) {
+             boolean Auth = true;
+            }
+        }
+        while(Auth == true) {
+
+        }
+
+
+
+
     }
 }
